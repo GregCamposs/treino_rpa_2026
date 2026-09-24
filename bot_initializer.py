8f42ad7c163304475958419b7e0da8d0aa5e61fa
Gregório Campos <gregoriodecamposs@gmail.com>
2026-09-24 14:32:06 -0300

    treino: lab01 concluído

---
+++ bot_initializer.py
@@ -0,0 +1,9 @@
+BOT_NAME = "Robô Inicializador"
+MAX_RETRIES = 3
+EXECUTION_TIMEOUT = 10.5
+IS_PRODUCTION = True
+
+print(f"Nome: {BOT_NAME} | Tipo:  {type(BOT_NAME)}")
+print(f"Tentativas: {MAX_RETRIES} | Tipo: {type(MAX_RETRIES)}")
+print(f"Timeout: {EXECUTION_TIMEOUT} | Tipo: {type(EXECUTION_TIMEOUT)}")
+print(f"Produção: {IS_PRODUCTION} | Tipo: {type(IS_PRODUCTION)}")
