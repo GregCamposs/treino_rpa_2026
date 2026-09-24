877ddff35a58c9dfdd0bd5622d50439e02d3e558
Gregório Campos <gregoriodecamposs@gmail.com>
2026-09-24 15:13:27 -0300

    treino: exercício 02 de metricas do bot

---
+++ bot_metrics.py
@@ -0,0 +1,9 @@
+TASK_NAME = "Extracao_Relatorio_Vendas"
+PROCESSED_ITEMS = 150
+TOTAL_TIME_MINUTES = 2.5
+SUCCESS_STATUS = True
+
+print(f"Nome: {TASK_NAME} | Tipo: {type(TASK_NAME)}")
+print(f"Itens: {PROCESSED_ITEMS} | Tipo: {type(PROCESSED_ITEMS)}")
+print(f"Minutos: {TOTAL_TIME_MINUTES} | Tipo: {type(TOTAL_TIME_MINUTES)}")
+print(f"Sucesso: {SUCCESS_STATUS} | Tipo: {type(SUCCESS_STATUS)}")
